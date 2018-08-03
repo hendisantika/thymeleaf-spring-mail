44ecd2bc6f03e2c07a9adad591873975ada511cc
Hendi Santika <hendisantika@gmail.com>
2018-08-03 07:11:31 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/thymeleafspringmail/ThymeleafSpringMailApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.thymeleafspringmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafSpringMailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafSpringMailApplication.class, args);
+    }
+}
---
+++ src/test/java/com/hendisantika/thymeleafspringmail/ThymeleafSpringMailApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.thymeleafspringmail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThymeleafSpringMailApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
